d280c5fa4887db2fcce27ed8eec4d1f66aa04f2c
Busi, Manasa Deepthi <manasadeepthi.busi@swbc.com>
2024-06-15 15:02:29 -0500

    Add project files.

---
+++ Module2/Easy/PrintInvoiceCommandTestsWithAutoMoqer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using Moq.AutoMock;
+using NUnit.Framework;
+using TestableCodeDemos.Module2.Shared;
+
+namespace TestableCodeDemos.Module2.Easy
+{
+    [TestFixture]
+    public class PrintInvoiceCommandTestsWithAutoMocker
+    {
+        private PrintInvoiceCommand _command;
+        private AutoMocker _mocker;
+        private Invoice _invoice;
+
+        private const int InvoiceId = 1;
+        private const decimal Total = 4.50m;
+        private static readonly DateTime Date = new DateTime(2001, 2, 3);
+
+        [SetUp]
+        public void SetUp()
+        {
+            _invoice = new Invoice()
+            {
+                Id = InvoiceId,
+                Total = Total
+            };
+
+            _mocker = new AutoMocker();
+
+            _mocker.GetMock<IDatabase>()
+                .Setup(p => p.GetInvoice(InvoiceId))
+                .Returns(_invoice);
+
+            _mocker.GetMock<IDateTimeWrapper>()
+                .Setup(p => p.GetNow())
+                .Returns(Date);
+
+            _command = _mocker.CreateInstance<PrintInvoiceCommand>();
+        }
+
+        [Test]
+        [TestCase("Invoice ID: 1")]
+        [TestCase("Total: $4.50")]
+        [TestCase("Printed: 2/3/2001")]
+        public void TestExecuteShouldPrintLine(string line)
+        {
+            _command.Execute(InvoiceId);
+
+            _mocker.GetMock<IPrinter>()
+                .Verify(p => p.WriteLine(line),
+                    Times.Once);
+        }
+    }
+}
---
+++ Module4/Easy/PrintInvoiceCommandTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using Moq.AutoMock;
+using NUnit.Framework;
+using TestableCodeDemos.Module4.Shared;
+
+namespace TestableCodeDemos.Module4.Easy
+{
+    [TestFixture]
+    public class PrintInvoiceCommandTests
+    {
+        private PrintInvoiceCommand _command;
+        private AutoMocker _mocker;
+        private Invoice _invoice;
+
+        private const int InvoiceId = 1;
+        private const string UserName = "mrenze";
+
+        [SetUp]
+        public void SetUp()
+        {
+            _invoice = new Invoice();
+
+            _mocker = new AutoMocker();
+
+            _mocker.GetMock<IDatabase>()
+                .Setup(p => p.GetInvoice(InvoiceId))
+                .Returns(_invoice);
+
+            _mocker.GetMock<IIdentityService>()
+                .Setup(p => p.GetUserName())
+                .Returns(UserName);
+
+            _command = _mocker.CreateInstance<PrintInvoiceCommand>();
+        }
+
+        [Test]
+        public void TestExecuteShouldPrintInvoice()
+        {
+            _command.Execute(InvoiceId);
+
+            _mocker.GetMock<IInvoiceWriter>()
+                .Verify(p => p.Write(_invoice),
+                    Times.Once);
+        }
+
+        [Test]
+        public void TestExecuteShouldLastPrintedByToCurrentUserName()
+        {
+            _command.Execute(InvoiceId);
+
+            Assert.That(_invoice.LastPrintedBy,
+                Is.EqualTo(UserName));
+        }
+
+        [Test]
+        public void TestExecuteShouldSaveChangesToDatabase()
+        {
+            _command.Execute(InvoiceId);
+
+            _mocker.GetMock<IDatabase>()
+                .Verify(p => p.Save(),
+                    Times.Once);
+        }
+    }
+}
